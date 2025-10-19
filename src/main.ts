5899cf40f36a2e622eb41525fc4105136eb1eda6
peter <peterphongpak@gmail.com>
2025-10-19 17:03:13 +0700

    feat: now support env variables

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,9 @@ import * as fs from "node:fs";
 import * as path from "node:path";
 import {Logger} from "@lib/logger/Logger";
 import chalk from "chalk";
+import {configDotenv} from "dotenv";
 
-const VERSION = "0.0.1a"
+const VERSION = "0.0.2a"
 const readline = createInterface({
   input: process.stdin,
   output: process.stdout
@@ -18,9 +19,17 @@ console.log(chalk.gray("==================================================="))
 console.log(chalk.gray.bold(`IQ180 game server (${VERSION})`))
 console.log(chalk.gray(`provide the password to unlock embedded resources.`))
 console.log(chalk.gray.bold("==================================================="))
+const cwd = process.cwd()
+const dbExists = fs.existsSync(path.join(cwd, "db/"))
+if (!dbExists) {
+  fs.mkdirSync(path.join(cwd, "db"))
+}
 
-readline.question('🔑 Embedded key password:', pass => {
-  EmbeddedKeyManager.loadKey(pass).then((v) => {
+configDotenv()
+
+const key = process.env.EMBEDDED_KEY
+if (key) {
+  EmbeddedKeyManager.loadKey(key).then((v) => {
     if (!v) {
       console.error(chalk.red("unable to load key with the provided password"));
       process.exit(0);
@@ -35,24 +44,38 @@ readline.question('🔑 Embedded key password:', pass => {
       }
     }
   })
-});
+}else{
+  readline.question('🔑 Embedded key password:', pass => {
+    EmbeddedKeyManager.loadKey(pass).then((v) => {
+      if (!v) {
+        console.error(chalk.red("unable to load key with the provided password"));
+        process.exit(0);
+      }else{
+        try {
+          admin.initializeApp({
+            credential: admin.credential.cert(v as admin.ServiceAccount)
+          })
+          readline.close();
+        } catch (error) {
+          throw error;
+        }
+      }
+    })
+  });
+}
 
 readline.on("SIGINT",() => {
   process.exit(0);
 })
 
 readline.on('close', () => {
-  const cwd = process.cwd()
-  const dbExists = fs.existsSync(path.join(cwd, "db/"))
-  if (!dbExists) {
-    fs.mkdirSync(path.join(cwd, "db"))
-  }
   const recheck = fs.existsSync(path.join(cwd, "db/"))
   if (!recheck) {
     throw "unable to create db folder"
   }
 
   const cg = new CoreGame()
-  cg.startGameServer(8080)
+  const port = process.env.PORT
+  cg.startGameServer(port ? parseInt(port) : 8080)
   cg.startMonitoringStream()
 })
